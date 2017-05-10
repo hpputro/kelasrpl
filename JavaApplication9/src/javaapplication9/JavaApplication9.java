021e5327fd7df0db7bce0f0cede4f010e1926b2f
hanson pp <hanson.prihantoro@gmail.com>
2017-05-10 13:24:08 +0700

    HelloWorld

--- JavaApplication9/src/javaapplication9/JavaApplication9.java
+++ JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -16,6 +16,7 @@ public class JavaApplication9 {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Selamat Datang");
     }
     
 }
