64054c668cf20d3fd0cd2095a2a300395a20f248
hanson pp <hanson.prihantoro@gmail.com>
2017-05-10 13:33:05 +0700

    HelloWorld

--- JavaApplication9/src/javaapplication9/JavaApplication9.java
+++ JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -15,7 +15,7 @@ public class JavaApplication9 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Halo Dunia");
         System.out.println("Welcome Back");
     }
     
